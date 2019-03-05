b36b57ad5f07383e8decea761edd83c6891e6427
belbns <belbns@gmail.com>
2019-03-05 16:44:50 +0300

    2019-03-05 (PWA 3)

--- sw.js
+++ sw.js
@@ -1,9 +1,14 @@
 importScripts('js/sw-toolbox.js');
 
 toolbox.precache([
-    'companion.js',
+    'css/index.css',
+    'js/main.js',
+    'js/nipplejs.js',
+    'js/companion.js',
     'index.html',
-    'main.js',
-    'nipplejs.js',
-    'index.css',
 ]);
+
+toolbox.router.default = toolbox.networkFirst;
+toolbox.options.networkTimeoutSeconds = 5;
+
+toolbox.router.get('icons/*', toolbox.fastest);
